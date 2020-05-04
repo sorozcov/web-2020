d7c137fb96782193fa22e05d08be0ce041ff198a
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-05-04 12:56:08 -0600

    Agregamos las sagas de petOwners a la principal

--- ejemplos_redux/src/sagas/index.js
+++ ejemplos_redux/src/sagas/index.js
@@ -2,12 +2,16 @@ import { fork, all } from 'redux-saga/effects';
 
 import { watchLoginStarted } from './auth';
 import { watchSayHappyBirthday } from './happyBirthday';
+import { watchPetOwnersAddStarted,watchPetOwnersFetchStarted,watchPetOwnersDeleteStarted } from './petOwners';
 
 
 function* mainSaga() {
   yield all([
     fork(watchLoginStarted),
     fork(watchSayHappyBirthday),
+    fork(watchPetOwnersAddStarted),
+    fork(watchPetOwnersDeleteStarted),
+    fork(watchPetOwnersFetchStarted),
   ]);
 }
 
